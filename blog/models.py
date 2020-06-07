1ddd835651e2d0373eeb1954ae27e5c3cc3e1aec
Yampolskiy <v_yamp@mail.ru>
2020-06-07 17:13:02 +0300

    Добавление фото

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(upload_to="blog/", blank=True, verbose_name='Изображение')
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+
+from djangogirls import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -7,3 +10,4 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
